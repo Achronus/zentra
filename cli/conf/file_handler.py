d3ff7fd6058f366e75d4bd3f3b6b169af4bd67de
Ryan Partridge <rpartridge101@yahoo.co.uk>
2024-02-27 22:47:54 +0000

    Add `make_path_dirs()` with tests

--- cli/conf/file_handler.py
+++ cli/conf/file_handler.py
@@ -5,6 +5,11 @@ class FileHandler:
     def __init__(self, folder_path: str) -> None:
         self.folder_path = folder_path
 
+    def make_path_dirs(self) -> None:
+        """Creates the directories for `self.folder_path` if they don't already exist."""
+        if not os.path.exists(self.folder_path):
+            os.makedirs(self.folder_path)
+
     def check_folder_exists(self) -> bool:
         """Checks if the `self.folder_path` exists in the current working directory."""
         if os.path.exists(self.folder_path) and os.path.isdir(self.folder_path):
--- tests/test_fh.py
+++ tests/test_fh.py
@@ -11,10 +11,15 @@ class TestFileHandler:
 
     @pytest.fixture
     def dummy_fh(self, tmp_path) -> FileHandler:
-        return FileHandler(os.path.join(tmp_path, "test_folder"))
+        return FileHandler(os.path.join(tmp_path, "test_folder", "models"))
+
+    def test_make_path_dirs_valid(self, zentra_fh, zentra_path):
+        zentra_fh.make_path_dirs()
+
+        assert os.path.exists(zentra_path), "Error! Directories not created."
 
     def test_check_folder_does_exist(self, zentra_fh, zentra_path):
-        os.mkdir(zentra_path)
+        zentra_fh.make_path_dirs()
 
         assert (
             zentra_fh.check_folder_exists() is True
@@ -24,13 +29,15 @@ class TestFileHandler:
         assert dummy_fh.check_folder_exists() is False, "Error: Folder exists."
 
     def test_check_folder_is_empty(self, zentra_fh, zentra_path):
-        os.mkdir(zentra_path)
+        zentra_fh.make_path_dirs()
+
         assert (
             zentra_fh.check_folder_empty() is True
         ), f"Error in path, # files in folder '{os.path.basename(zentra_path)}': {len(os.listdir(zentra_path))}"
 
     def test_check_folder_is_not_empty(self, zentra_fh, zentra_path):
-        os.mkdir(zentra_path)
+        zentra_fh.make_path_dirs()
+
         open(os.path.join(zentra_path, "file1.py"), "w")
 
         assert (
@@ -38,13 +45,15 @@ class TestFileHandler:
         ), f"Error in path, # files in folder '{os.path.basename(zentra_path)}': {len(os.listdir(zentra_path))}"
 
     def test_get_python_files_has_files(self, zentra_fh, zentra_path):
-        os.mkdir(zentra_path)
+        zentra_fh.make_path_dirs()
+
         open(os.path.join(zentra_path, "file1.py"), "w")
 
         assert len(zentra_fh.get_python_files()) > 0, "Error! No files exist."
 
-    def test_get_python_files_has_no_files(self, zentra_fh, zentra_path):
-        os.mkdir(zentra_path)
+    def test_get_python_files_has_no_files(self, zentra_fh):
+        zentra_fh.make_path_dirs()
+
         file_count = len(zentra_fh.get_python_files())
 
         assert file_count == 0, f"Error! {file_count} files exist."

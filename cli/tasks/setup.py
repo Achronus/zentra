af1bb5b0716a42c5d1883337181fdd793bffe007
Ryan Partridge <rpartridge101@yahoo.co.uk>
2024-02-27 22:52:37 +0000

    Add `init_app()` to `setup.py` with helper placeholders

--- cli/tasks/setup.py
+++ cli/tasks/setup.py
@@ -1,6 +1,10 @@
 import os
+import typer
 
+from ..conf.constants import PASS, StatusCode
 from ..conf.file_handler import FileHandler
+
+from rich import print
 
 
 class Setup:
@@ -12,3 +16,25 @@ class Setup:
 
         self.zentra_models_exists = self.fh.check_folder_exists()
         self.zentra_models_empty = self.fh.check_folder_empty()
+
+    def __check_for_model(self) -> bool:
+        """Helper function to check for component models in the `zentra` folder."""
+        pass
+
+    def create_demo_component(self) -> None:
+        """Creates an example Zentra component."""
+        pass
+
+    def init_app(self) -> None:
+        """Performs configuration to initialise application with `zentra`."""
+        if self.zentra_models_exists:
+            if self.zentra_models_empty:
+                self.create_demo_component()
+            else:
+                if self.fh.check_component_model_exists():
+                    print(
+                        f"{PASS} Application already configured with components! Use [green]zentra generate[/green] to create them!"
+                    )
+                    typer.Exit(code=StatusCode.CONFIGURED)
+        else:
+            os.mkdir(self.folder_path)

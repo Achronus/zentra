4a3bb881e867a46a3b89dd7e23a3417bc8a92059
Ryan Partridge <rpartridge101@yahoo.co.uk>
2024-02-28 08:57:00 +0000

    Add `FormField` component

--- zentra/ui/__init__.py
+++ zentra/ui/__init__.py
@@ -1,10 +1,20 @@
 from zentra.core import Component
 
 
+class FormField(Component):
+    """A Zentra model for the `shadcn/ui` FormField inside the Form component."""
+
+    label: str
+    component: Component
+    disabled: bool = False
+    description: str = None
+    message: bool = True
+
+
 class Form(Component):
     """A Zentra model for the `shadcn/ui` Form component."""
 
-    fields: list[Component]
+    fields: list[FormField]
 
 
 class FileUpload(Component):

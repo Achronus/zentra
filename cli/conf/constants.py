cef4da960333087d1e473c725b238cd5305741d0
Ryan Partridge <rpartridge101@yahoo.co.uk>
2024-03-04 09:38:51 +0000

    Add zentra filepaths as constants

--- cli/conf/constants.py
+++ cli/conf/constants.py
@@ -15,12 +15,31 @@ FAIL = "[red]\u274c[/red]"
 PARTY = ":party_popper:"
 
 
-# Filepaths
+# Core folder names and URLS
 FOLDER_NAME = "zentra"
 DOCS_URL = "#"
 
 
 class ZentaFilepaths:
-    ROOT = os.path.join(os.getcwd(), FOLDER_NAME)
-    MODELS = os.path.join(ROOT, "models")
-    GENERATED = os.path.join(ROOT, "generated")
+    ROOT = os.path.join(os.getcwd(), FOLDER_NAME)  # (cwd)/zentra
+    MODELS = os.path.join(ROOT, "models")  # (cwd)/zentra/models
+    GENERATED = os.path.join(ROOT, "generated")  # (cwd)/zentra/generated
+
+
+class ZentraGeneratedFilepaths:
+    # (cwd)/zentra/generated
+    ROOT = ZentaFilepaths.GENERATED
+    COMPONENTS = os.path.join(ROOT, "components")  # ../components
+    ZENTRA = os.path.join(COMPONENTS, "zentra")  # ../zentra
+
+
+class ZentraUIFilepaths:
+    # (cwd)/zentra/generated/components/zentra/ui
+    ROOT = os.path.join(ZentraGeneratedFilepaths.ZENTRA, "ui")
+    BASE = os.path.join(ROOT, "base")  # ../base
+
+    CONTROL = os.path.join(BASE, "control")  # ../control
+    MODAL = os.path.join(BASE, "modal")  # ../modal
+    NAVIGATION = os.path.join(BASE, "navigation")  # ../navigation
+    NOTIFICATION = os.path.join(BASE, "notification")  # ../notification
+    PRESENTATION = os.path.join(BASE, "presentation")  # ../presentation
---
+++ tests/constants/test_filepaths.py
@@ -0,0 +1,70 @@
+import os
+import unittest
+
+from cli.conf.constants import (
+    ZentaFilepaths,
+    ZentraGeneratedFilepaths,
+    ZentraUIFilepaths,
+)
+
+
+class TestZentaFilepaths(unittest.TestCase):
+    def test_root_path(self):
+        self.assertEqual(ZentaFilepaths.ROOT, os.path.join(os.getcwd(), "zentra"))
+
+    def test_models_path(self):
+        self.assertEqual(
+            ZentaFilepaths.MODELS, os.path.join(os.getcwd(), "zentra", "models")
+        )
+
+    def test_generated_path(self):
+        self.assertEqual(
+            ZentaFilepaths.GENERATED, os.path.join(os.getcwd(), "zentra", "generated")
+        )
+
+
+class TestZentraGeneratedFilepaths(unittest.TestCase):
+    def test_root_path(self):
+        self.assertEqual(ZentraGeneratedFilepaths.ROOT, ZentaFilepaths.GENERATED)
+
+    def test_components_path(self):
+        self.assertEqual(
+            ZentraGeneratedFilepaths.COMPONENTS,
+            os.path.join(ZentaFilepaths.GENERATED, "components"),
+        )
+
+    def test_zentra_path(self):
+        self.assertEqual(
+            ZentraGeneratedFilepaths.ZENTRA,
+            os.path.join(ZentraGeneratedFilepaths.COMPONENTS, "zentra"),
+        )
+
+
+class TestZentraUIFilepaths(unittest.TestCase):
+    def test_root_path(self):
+        expected_path = os.path.join(ZentraGeneratedFilepaths.ZENTRA, "ui")
+        self.assertEqual(ZentraUIFilepaths.ROOT, expected_path)
+
+    def test_base_path(self):
+        expected_path = os.path.join(ZentraUIFilepaths.ROOT, "base")
+        self.assertEqual(ZentraUIFilepaths.BASE, expected_path)
+
+    def test_control_path(self):
+        expected_path = os.path.join(ZentraUIFilepaths.BASE, "control")
+        self.assertEqual(ZentraUIFilepaths.CONTROL, expected_path)
+
+    def test_modal_path(self):
+        expected_path = os.path.join(ZentraUIFilepaths.BASE, "modal")
+        self.assertEqual(ZentraUIFilepaths.MODAL, expected_path)
+
+    def test_navigation_path(self):
+        expected_path = os.path.join(ZentraUIFilepaths.BASE, "navigation")
+        self.assertEqual(ZentraUIFilepaths.NAVIGATION, expected_path)
+
+    def test_notification_path(self):
+        expected_path = os.path.join(ZentraUIFilepaths.BASE, "notification")
+        self.assertEqual(ZentraUIFilepaths.NOTIFICATION, expected_path)
+
+    def test_presentation_path(self):
+        expected_path = os.path.join(ZentraUIFilepaths.BASE, "presentation")
+        self.assertEqual(ZentraUIFilepaths.PRESENTATION, expected_path)

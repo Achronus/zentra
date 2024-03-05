516d103883a7a33d86d5c4b4c7b900aac85beeca
Ryan Partridge <rpartridge101@yahoo.co.uk>
2024-03-05 11:43:20 +0000

    Refactor filename retrieval func for any file + only filename

--- cli/conf/extract.py
+++ cli/conf/extract.py
@@ -6,8 +6,7 @@ def get_filenames_in_subdir(filepath: str) -> list[str]:
     filenames = []
     for root, dirs, files in os.walk(filepath):
         for file in files:
-            if file.endswith("py"):
-                filenames.append(os.path.join(root, file))
+            filenames.append(file)
     return filenames
 
 

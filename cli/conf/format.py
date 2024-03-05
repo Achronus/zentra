20966b35604fcece8b3a60226559d281938f5293
Ryan Partridge <rpartridge101@yahoo.co.uk>
2024-03-05 03:30:24 +0000

    Add `name_to_camel_case`

---
+++ cli/conf/format.py
@@ -0,0 +1,12 @@
+import re
+
+
+def name_to_camel_case(name: str) -> str:
+    """
+    Converts a name from camel case to a lowercase dashed format.
+
+    Example:
+    - AspectRatio -> aspect-ratio
+    """
+    converted_name = re.sub("([a-z0-9])([A-Z])", r"\1-\2", name)
+    return converted_name.lower()
---
+++ tests/conf/test_format.py
@@ -0,0 +1,13 @@
+import re
+from hypothesis import given
+from hypothesis.strategies import text, characters
+
+from cli.conf.format import name_to_camel_case
+
+
+@given(text(alphabet=characters(whitelist_categories=("Lu", "Ll", "Nd")), min_size=10))
+def test_name_to_camel_case_hypothesis(input_string):
+    result = name_to_camel_case(input_string)
+
+    expected_result = re.sub("([a-z0-9])([A-Z])", r"\1-\2", input_string).lower()
+    assert result == expected_result

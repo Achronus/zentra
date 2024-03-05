558a98f7018724995193bcbcb58066286eedd86f
Ryan Partridge <rpartridge101@yahoo.co.uk>
2024-03-05 02:05:11 +0000

    Add `Page` schema method

--- zentra/core/__init__.py
+++ zentra/core/__init__.py
@@ -15,7 +15,39 @@ class Component(BaseModel):
 class Page(BaseModel):
     """A Zentra model for a single webpage of React components."""
 
+    name: str
     components: list[Component]
+
+    def get_schema(self, node: BaseModel = None) -> dict:
+        """Returns a JSON tree of the `Page` components as nodes with a type (the component name) and its attributes (attrs)."""
+        if node is None:
+            node = self
+
+        formatted_schema = {
+            "node": {
+                "type": node.__class__.__name__,
+                "attrs": node.model_dump(),
+            }
+        }
+
+        valid_attrs = ["content", "components", "fields"]
+        components_attr = next(
+            (attr for attr in valid_attrs if hasattr(node, attr)), None
+        )
+
+        if components_attr is not None:
+            children = getattr(node, components_attr)
+
+            # Handle leaf nodes
+            if not isinstance(children, list):
+                children = [children]
+
+            if children:
+                formatted_schema["children"] = [
+                    self.get_schema(child) for child in children
+                ]
+
+        return formatted_schema
 
 
 class Icon(BaseModel):

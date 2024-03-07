dc01d796e575a70eb8e5ff0f2fd6dc32fb78bd1d
Ryan Partridge <rpartridge101@yahoo.co.uk>
2024-03-07 03:35:51 +0000

    Add `GenerateController` tests + refactor others to classes

--- tests/test_controller.py
+++ tests/test_controller.py
@@ -1,5 +1,128 @@
 import pytest
+from cli.conf.constants import LocalUploadthingFilepaths
+from cli.conf.format import name_from_camel_case
+
+from cli.tasks.controllers.path import FolderDoesNotExistController
+from cli.tasks.controllers.generate import GenerateController
+from zentra.core import Page, Zentra
+from zentra.ui import FileUpload, Form, FormField
+from zentra.ui.control import Input
+from zentra.ui.notification import AlertDialog
+from zentra.ui.presentation import Card
 
 
-def test_make_path_success(folder_controller):
-    assert folder_controller.make_path() is True
+class TestFolderDoesNotExistController:
+    @pytest.fixture
+    def controller(self) -> FolderDoesNotExistController:
+        return FolderDoesNotExistController()
+
+    def test_make_path_success(self, controller: FolderDoesNotExistController):
+        assert controller.make_path() is True
+
+
+class TestNameStorage:
+    def test_nextjs_project_false(self):
+        nextjs_project = False
+
+        result = (
+            LocalUploadthingFilepaths.BASE_NEXTJS
+            if nextjs_project
+            else LocalUploadthingFilepaths.BASE_BASIC
+        )
+        assert result == LocalUploadthingFilepaths.BASE_BASIC
+
+    def test_nextjs_project_true(self):
+        nextjs_project = True
+
+        result = (
+            LocalUploadthingFilepaths.BASE_NEXTJS
+            if nextjs_project
+            else LocalUploadthingFilepaths.BASE_BASIC
+        )
+        assert result == LocalUploadthingFilepaths.BASE_NEXTJS
+
+
+class TestGenerateController:
+    @pytest.fixture
+    def page(self) -> Page:
+        return Page(
+            name="AgencyDetails",
+            components=[
+                AlertDialog(
+                    name="agencyAlertDialog",
+                    content=[
+                        Card(
+                            name="agencyInfo",
+                            title="Agency Information",
+                            description="Let's create an agency for your business. You can edit agency settings later from the agency settings tab.",
+                            content=[
+                                Form(
+                                    name="agencyForm",
+                                    fields=[
+                                        FormField(
+                                            name="agencyLogo",
+                                            label="Agency Logo",
+                                            content=FileUpload(name="agencyLogo"),
+                                        ),
+                                        FormField(
+                                            name="name",
+                                            label="Agency Name",
+                                            content=Input(
+                                                name="name",
+                                                label="Agency Name",
+                                                placeholder="Your Agency Name",
+                                            ),
+                                        ),
+                                    ],
+                                ),
+                            ],
+                        ),
+                    ],
+                ),
+            ],
+        )
+
+    @pytest.fixture
+    def zentra(self, page: Page) -> Zentra:
+        zentra = Zentra()
+        zentra.register([page])
+        return zentra
+
+    @pytest.fixture
+    def controller(self, zentra: Zentra) -> GenerateController:
+        return GenerateController(zentra)
+
+    class TestExtractModels:
+        @pytest.fixture
+        def names(self, zentra: Zentra) -> list[str]:
+            return [
+                f"{name_from_camel_case(name)}.tsx" for name in zentra.component_names
+            ]
+
+        def test_formatted_names(self, names: list[str]):
+            valid_names = [
+                "alert-dialog.tsx",
+                "form.tsx",
+                "card.tsx",
+                "file-upload.tsx",
+                "input.tsx",
+            ]
+
+            assert len(names) == len(valid_names)
+
+        def test_ut_to_generate(self, controller: GenerateController):
+            controller.extract_models()
+            result = controller.storage.UT_TO_GENERATE
+            valid = ["file-upload.tsx"]
+            assert len(result) == len(valid)
+
+        def test_ui_to_generate(self, controller: GenerateController):
+            controller.extract_models()
+            result = controller.storage.UI_TO_GENERATE
+            valid = [
+                "alert-dialog.tsx",
+                "form.tsx",
+                "card.tsx",
+                "input.tsx",
+            ]
+            assert len(result) == len(valid)

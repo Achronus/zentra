1ae5c406bdbe1b7609eb564641635f7ffe01408d
Ryan Partridge <rpartridge101@yahoo.co.uk>
2024-03-10 06:47:41 +0000

    Add comments to `zentra/models` config for user clarity

--- cli/zentra_config/__init__.py
+++ cli/zentra_config/__init__.py
@@ -1,10 +1,14 @@
 from zentra.core import Component, Page, Zentra
 
 # Web pages that contain multiple React components
-page_map: list[Page] = []
+page_map: list[Page] = [
+    # Custom pages here...
+]
 
 # Single components that are not in pages
-standalone_components: list[Component] = []
+standalone_components: list[Component] = [
+    # Custom components here...
+]
 
 # Setup the application
 zentra = Zentra()

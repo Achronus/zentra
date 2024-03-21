e41b5a6c4ced24d3bfd75241b6bd565f1f27da28
Ryan Partridge <rpartridge101@yahoo.co.uk>
2024-03-21 22:39:02 +0000

    Update page demo with better example

--- cli/zentra_config/_demo/agency_details.py
+++ cli/zentra_config/_demo/agency_details.py
@@ -1,9 +1,64 @@
 from zentra.core import Page
-from zentra.ui import FileUpload, Form, FormField
-from zentra.ui.control import Input
+from zentra.ui import Form, FormField
+from zentra.ui.control import Input, Switch
 from zentra.ui.notification import AlertDialog
 from zentra.ui.presentation import Card
+from zentra.uploadthing import FileUpload
 
+
+form_fields = [
+    FormField(
+        name="agencyLogo",
+        label="Agency Logo",
+        content=FileUpload(),
+    ),
+    [
+        FormField(
+            name="name",
+            label="Agency Name",
+            content=Input(type="text", placeholder="Your Agency Name"),
+        ),
+        FormField(
+            name="companyEmail",
+            label="Agency Email",
+            content=Input(type="email", placeholder="Email"),
+        ),
+    ],
+    FormField(
+        name="companyPhone",
+        label="Agency Phone Number",
+        content=Input(type="tel", placeholder="Phone"),
+    ),
+    FormField(
+        name="whiteLabel",
+        label="White Label Mode",
+        description="Turning on White label mode will show your agency logo to all sub accounts by default. You can override this behaviour through sub account settings.",
+        content=Switch(),
+        message=False,
+    ),
+    FormField(
+        name="address",
+        label="Address",
+        content=Input(type="text", placeholder="123 st..."),
+    ),
+    [
+        FormField(
+            name="city",
+            label="City",
+            content=Input(type="text", placeholder="City"),
+        ),
+        FormField(
+            name="state",
+            label="State",
+            content=Input(type="text", placeholder="State"),
+        ),
+        FormField(
+            name="zipCode",
+            label="Zipcode",
+            content=Input(type="text", placeholder="Zipcode"),
+        ),
+    ],
+]
 
 agency_details = Page(
     name="AgencyDetails",
@@ -18,33 +73,8 @@ agency_details = Page(
                     content=[
                         Form(
                             name="agencyForm",
-                            layout=[1, 2],
-                            fields=[
-                                FormField(
-                                    name="agencyLogo",
-                                    label="Agency Logo",
-                                    content=FileUpload(),
-                                ),
-                                FormField(
-                                    name="name",
-                                    label="Agency Name",
-                                    content=Input(
-                                        type="text",
-                                        label="Agency Name",
-                                        placeholder="Your Agency Name",
-                                    ),
-                                ),
-                                FormField(
-                                    name="companyEmail",
-                                    label="Agency Email",
-                                    content=Input(
-                                        type="email",
-                                        label="Account Email",
-                                        placeholder="Email",
-                                        read_only=True,
-                                    ),
-                                ),
-                            ],
+                            fields=form_fields,
+                            btn_text="Save Agency Information",
                         ),
                     ],
                 ),

cccb3813a1e7743ae7e473eb9eb655433b7a0e54
Ryan Partridge <rpartridge101@yahoo.co.uk>
2024-04-11 10:41:14 +0100

    Refactor `FileUpload` parameter names to PEP 8 format

--- zentra/uploadthing/__init__.py
+++ zentra/uploadthing/__init__.py
@@ -34,7 +34,7 @@ class FileUpload(Component, Uploadthing):
     """A Zentra model for the [uploadthing](https://uploadthing.com/) FileUpload fields.
 
     Parameters:
-    - `apiEndpoint` (`str, optional`) - the API endpoint to use. Defaults to `media`. API endpoints are stored in `frontend/src/lib/core.ts -> uploadFileRouter`. We advise you leave this as `media` until you update your `frontend` files later. You can find more information about this in the [uploadthing docs](https://docs.uploadthing.com/getting-started/appdir#set-up-a-filerouter)
+    - `api_endpoint` (`str, optional`) - the API endpoint to use. Defaults to `media`. API endpoints are stored in `frontend/src/lib/core.ts -> uploadFileRouter`. We advise you leave this as `media` until you update your `frontend` files later. You can find more information about this in the [uploadthing docs](https://docs.uploadthing.com/getting-started/appdir#set-up-a-filerouter)
     """
 
-    apiEndpoint: str = "media"
+    api_endpoint: str = "media"

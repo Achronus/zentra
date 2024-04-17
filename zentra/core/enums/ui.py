bafb2f622129036e5e505e613ea3a2f8263574d0
Ryan Partridge <rpartridge101@yahoo.co.uk>
2024-04-17 08:08:40 +0100

    Refactor `ScrollAreaData` model to `DataArray` for simplicity
    Convert 'ScrollAreaData' into generic object to use across whole tool. Also, move to 'zentra.core' for clarity and global accessibility.

--- zentra/core/enums/ui.py
+++ zentra/core/enums/ui.py
@@ -1,10 +1,4 @@
 from enum import Enum, IntEnum
-from typing import Any
-
-from pydantic import BaseModel, Field, field_validator
-from pydantic_core import PydanticCustomError
-
-from zentra.core import LOWER_CAMELCASE_SINGLE_WORD, has_valid_pattern
 
 
 class LibraryType(str, Enum):
@@ -66,59 +60,3 @@ class InputOTPPatterns(str, Enum):
 class Orientation(str, Enum):
     VERTICAL = "vertical"
     HORIZONTAL = "horizontal"
-
-
-class ScrollAreaData(BaseModel):
-    """
-    A storage container for the `ScrollArea` data attribute.
-
-    Parameters:
-    - `name` (`string`) - the name of the `data` object. E.g., 'works'. Must be `lowercase` or `camelCase` and a maximum of `30` characters
-    - `parameter` (`string`) - The map `parameter` name. E.g., 'artwork'. Must be `lowercase` or `camelCase` and a maximum of `30` characters
-    - `data` (`list[dict[string, Any]]`) - A list of dictionaries containing information to pass into the `ScrollArea` component that is iterated over using a `map` function. Each dictionary must have the same key values and values of the same type
-    """
-
-    name: str = Field(min_length=1, max_length=30)
-    parameter: str = Field(min_length=1, max_length=30)
-    data: list[dict[str, Any]]
-
-    @field_validator("data")
-    def validate_data(cls, data: list[dict[str, Any]]) -> list[dict[str, Any]]:
-        if len(data) == 0 or len(data[0]) == 0:
-            raise PydanticCustomError(
-                "missing_data",
-                "No data exists in the list",
-                dict(wrong_value=data),
-            )
-
-        reference_dict = data[0]
-        for idx, d in enumerate(data[1:], start=1):
-            if set(d.keys()) != set(reference_dict.keys()):
-                raise PydanticCustomError(
-                    "invalid_dictionary_keys",
-                    f"position: 2.{idx} -> '{d.keys()} != {reference_dict.keys()}'\n",
-                    dict(wrong_value=d, full_data=data),
-                )
-
-            for key, value in reference_dict.items():
-                if type(d[key]) != type(value):
-                    raise PydanticCustomError(
-                        "invalid_value_type",
-                        f"position: 2.{idx} -> '{type(d[key])} ({d[key]}) != {type(value)} ({value})'\n",
-                        dict(wrong_value=d, full_data=data),
-                    )
-
-        return data
-
-    @field_validator("name", "parameter")
-    def validate_name_parameter(cls, v: str) -> str:
-        result = has_valid_pattern(pattern=LOWER_CAMELCASE_SINGLE_WORD, value=v)
-
-        if not result:
-            raise PydanticCustomError(
-                "string_pattern_mismatch",
-                f"'{v}'. Must be 'lowercase' or 'camelCase', a single word and a maximum of '30' characters\n",
-                dict(wrong_value=v, pattern=LOWER_CAMELCASE_SINGLE_WORD),
-            )
-
-        return v

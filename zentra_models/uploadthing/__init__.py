a86e1cf7413f975597b7c73ffa3df6ae163e9444
Ryan Partridge <rpartridge101@yahoo.co.uk>
2024-07-01 18:30:11 +0100

    Remove retrieval logic from `uploadthing`

--- zentra_models/uploadthing/__init__.py
+++ zentra_models/uploadthing/__init__.py
@@ -1,7 +1,3 @@
-from zentra_models.cli.conf.constants import GITHUB_COMPONENTS_DIR
-from zentra_models.cli.conf.types import LibraryNamePairs
-from zentra_models.cli.templates.retrieval import GithubContentRetriever
-
 from zentra_models.core import Component
 from zentra_models.core.enums.ui import LibraryType
 
@@ -9,26 +5,9 @@ from zentra_models.core.enums.ui import LibraryType
 class Uploadthing:
     """A Zentra model for all [uploadthing](https://uploadthing.com/) components."""
 
-    def __init__(self, core_folder: str = "lib") -> None:
-        self.core_folder = core_folder
-        self.core_path = f"{GITHUB_COMPONENTS_DIR}/{self.library}/{core_folder}"
-        self.retriever = GithubContentRetriever(url=self.core_path)
-
     @property
     def library(self) -> str:
         return LibraryType.UPLOADTHING.value
-
-    def core_file_urls(self) -> tuple[str, list[str]]:
-        """Provides the GitHub URL root of the core files needed for the library and the names of the files inside it.
-
-        Returns: `(core_path, [core_filename, ...])` ->
-        - `core_file_urls` - the core filepath URLs to the files found in the GitHub repository
-        '"""
-        return self.core_path, self.retriever.filenames()
-
-    def core_file_pairs(self) -> LibraryNamePairs:
-        """Provides a list of '(library_name, filename)' pairs for the core files needed for the library."""
-        return [(self.core_folder, name) for name in self.retriever.filenames()]
 
 
 class FileUpload(Component, Uploadthing):

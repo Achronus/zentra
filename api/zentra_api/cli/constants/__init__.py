bf4ee6c0d869f948fa474bd31d1c3ed509a55624
Ryan Partridge <rpartridge101@yahoo.co.uk>
2024-07-23 15:45:00 +0100

    Add init command setup
    - Add boilerplate for 'init' and 'add' commands
    - Configure basic setup for 'MessageHandler'

---
+++ api/zentra_api/cli/constants/__init__.py
@@ -0,0 +1,31 @@
+from enum import Enum
+from rich.console import Console
+
+
+console = Console()
+
+
+# Core URLs
+DOCS_URL = "https://zentra.achronus.dev"
+GITHUB_ROOT = "https://github.com/Achronus/zentra"
+GITHUB_ISSUES_URL = f"{GITHUB_ROOT}/issues"
+
+GETTING_STARTED_URL = f"{DOCS_URL}/starting/api/"
+ERROR_GUIDE_URL = f"{DOCS_URL}/help/errors/"
+
+
+# Custom print emoji's
+PASS = "[green]\u2713[/green]"
+FAIL = "[red]\u274c[/red]"
+PARTY = ":party_popper:"
+MAGIC = ":sparkles:"
+
+
+class SetupSuccessCodes(Enum):
+    COMPLETE = 10
+    ALREADY_CONFIGURED = 11
+
+
+class CommonErrorCodes(Enum):
+    TEST_ERROR = -1
+    UNKNOWN_ERROR = 1000
---
+++ api/zentra_api/cli/main.py
@@ -0,0 +1,68 @@
+from typing import Annotated
+import typer
+
+from zentra_api.cli.constants import console
+from zentra_api.cli.constants.enums import AddItem, DefaultFolderOptions
+from zentra_api.cli.constants.message import MSG_MAPPER, MessageHandler
+
+
+init_command = typer.style("init", typer.colors.YELLOW)
+add_command = typer.style("add", typer.colors.YELLOW)
+
+app = typer.Typer(
+    help=f"Welcome to Zentra API! Create a project with {init_command} or add something with {add_command}.",
+    rich_markup_mode="rich",
+    pretty_exceptions_enable=True,
+)
+
+msg_handler = MessageHandler(console, MSG_MAPPER)
+
+
+@app.command("init")
+def init(
+    project_name: Annotated[
+        str,
+        typer.Argument(
+            help="The name of the FastAPI project to create",
+            show_default=False,
+        ),
+    ],
+) -> None:
+    """Creates a new FastAPI project in a folder called <PROJECT_NAME>."""
+    try:
+        pass
+
+    except typer.Exit as e:
+        msg_handler.msg(e)
+
+
+@app.command("add")
+def add(
+    item: Annotated[
+        AddItem,
+        typer.Argument(
+            help="The type of item to add.",
+            show_default=False,
+        ),
+    ],
+    folder: Annotated[
+        str,
+        typer.Option(
+            help="The folder to add the item to. E.g., 'auth'. When <item='route'>, defaults to 'api'. When <item='test'> defaults to 'tests'.",
+            show_default=False,
+        ),
+    ] = None,
+) -> None:
+    """Adds a new <ITEM> into the project in <FOLDER> with <NAME>."""
+    try:
+        if not folder:
+            folder = getattr(DefaultFolderOptions, item.upper())
+
+        print(folder)
+
+    except typer.Exit as e:
+        msg_handler.msg(e)
+
+
+if __name__ == "__main__":
+    app()

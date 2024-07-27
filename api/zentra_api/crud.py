2d299bc0bba7653916d2b9ba8fe610358306c37c
Ryan Partridge <rpartridge101@yahoo.co.uk>
2024-07-27 22:05:36 +0100

    Add `UserCRUD` class with methods

--- api/zentra_api/crud.py
+++ api/zentra_api/crud.py
@@ -61,3 +61,62 @@ class CRUD(BaseModel):
             db.commit()
 
         return result
+
+
+class UserCRUD(BaseModel):
+    """
+    Handles create, read, update, and delete operations for the `User` database table.
+
+    Parameters:
+    - `model` (`Base`) - the `User` database table to operate on
+    """
+
+    model: Type
+
+    model_config = ConfigDict(arbitrary_types_allowed=True)
+
+    def _get(self, db: Session, attr: str, value: str) -> Any:
+        """Utility method for getting a single user."""
+        return db.query(self.model).filter(getattr(self.model, attr) == value).first()
+
+    def create(self, db: Session, data: dict) -> Any:
+        """Adds a user to the table."""
+        item = self.model(**data)
+        db.add(item)
+        db.commit()
+        db.refresh(item)
+        return item
+
+    def get_by_username(self, db: Session, username: str) -> Any | None:
+        """Retrieves a single user from the table by username."""
+        return self._get(db, "username", username)
+
+    def get_by_id(self, db: Session, id: int) -> Any | None:
+        """Retrieves a single user from the table by ID."""
+        return self._get(db, "id", id)
+
+    def update(self, db: Session, id: int, data: BaseModel) -> Any | None:
+        """Updates a users details in the table."""
+        result = self._get(db, "id", id)
+
+        if not result:
+            return None
+
+        update_data = data.model_dump(exclude_unset=True)
+
+        for field, value in update_data.items():
+            setattr(result, field, value)
+
+        db.commit()
+        db.refresh(result)
+        return result
+
+    def delete(self, db: Session, id: int) -> Any | None:
+        """Deletes an item from the table."""
+        result = self._get(db, "id", id)
+
+        if result:
+            db.delete(result)
+            db.commit()
+
+        return result

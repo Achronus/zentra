3720f7b3424d09b8aad6cee533c66469c4f57474
Ryan Partridge <rpartridge101@yahoo.co.uk>
2024-07-28 14:35:55 +0100

    Refactor `env` file and add `UserDetails` table
    - Remove redundant 'env' variables - created dynamically
    - Add 'UserDetails' table to 'models.py'

--- api/zentra_api/cli/template/app/models.py
+++ api/zentra_api/cli/template/app/models.py
@@ -7,16 +7,26 @@ from zentra_api.crud import CRUD, UserCRUD
 
 
 class User(SETTINGS.SQL.Base):
-    """A model of the user table."""
+    """A model of the `User` table."""
 
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True)
     username = Column(String, unique=True, index=True)
-    email = Column(String, unique=True)
-    full_name = Column(String)
     password = Column(String)
     is_active = Column(Boolean, default=True)
+
+
+class UserDetails(SETTINGS.SQL.Base):
+    """A model of the `UserDetails` table."""
+
+    __tablename__ = "user_details"
+
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey("users.id"))
+    email = Column(String, unique=True)
+    phone = Column(String, unique=True)
+    full_name = Column(String)
 
 
 SETTINGS.SQL.create_all()
@@ -27,6 +37,7 @@ class DBConnections:
 
     def __init__(self) -> None:
         self.user = UserCRUD(model=User)
+        self.user_details = CRUD(model=UserDetails)
 
 
 CONNECT = DBConnections()

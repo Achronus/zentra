931296d7ff186338e5b7d03f88968b003e9b4ef7
Ryan Partridge <rpartridge101@yahoo.co.uk>
2024-07-29 10:24:20 +0100

    Refactor `HTTPMessage` tests to confirm `status`

--- api/tests/responses/test_messages.py
+++ api/tests/responses/test_messages.py
@@ -20,248 +20,372 @@ class TestHTTPMessage:
 class TestHttpMapping:
     @staticmethod
     def test_100_valid():
-        assert isinstance(HTTP_MSG_MAPPING[100], HTTPMessage)
+        msg = HTTP_MSG_MAPPING[100]
+        assert isinstance(msg, HTTPMessage)
+        assert msg.status == "info"
 
     @staticmethod
     def test_101_valid():
-        assert isinstance(HTTP_MSG_MAPPING[101], HTTPMessage)
+        msg = HTTP_MSG_MAPPING[101]
+        assert isinstance(msg, HTTPMessage)
+        assert msg.status == "info"
 
     @staticmethod
     def test_102_valid():
-        assert isinstance(HTTP_MSG_MAPPING[102], HTTPMessage)
+        msg = HTTP_MSG_MAPPING[102]
+        assert isinstance(msg, HTTPMessage)
+        assert msg.status == "info"
 
     @staticmethod
     def test_103_valid():
-        assert isinstance(HTTP_MSG_MAPPING[103], HTTPMessage)
+        msg = HTTP_MSG_MAPPING[103]
+        assert isinstance(msg, HTTPMessage)
+        assert msg.status == "info"
 
     @staticmethod
     def test_200_valid():
-        assert isinstance(HTTP_MSG_MAPPING[200], HTTPMessage)
+        msg = HTTP_MSG_MAPPING[200]
+        assert isinstance(msg, HTTPMessage)
+        assert msg.status == "success"
 
     @staticmethod
     def test_201_valid():
-        assert isinstance(HTTP_MSG_MAPPING[201], HTTPMessage)
+        msg = HTTP_MSG_MAPPING[201]
+        assert isinstance(msg, HTTPMessage)
+        assert msg.status == "success"
 
     @staticmethod
     def test_202_valid():
-        assert isinstance(HTTP_MSG_MAPPING[202], HTTPMessage)
+        msg = HTTP_MSG_MAPPING[202]
+        assert isinstance(msg, HTTPMessage)
+        assert msg.status == "success"
 
     @staticmethod
     def test_203_valid():
-        assert isinstance(HTTP_MSG_MAPPING[203], HTTPMessage)
+        msg = HTTP_MSG_MAPPING[203]
+        assert isinstance(msg, HTTPMessage)
+        assert msg.status == "success"
 
     @staticmethod
     def test_204_valid():
-        assert isinstance(HTTP_MSG_MAPPING[204], HTTPMessage)
+        msg = HTTP_MSG_MAPPING[204]
+        assert isinstance(msg, HTTPMessage)
+        assert msg.status == "success"
 
     @staticmethod
     def test_205_valid():
-        assert isinstance(HTTP_MSG_MAPPING[205], HTTPMessage)
+        msg = HTTP_MSG_MAPPING[205]
+        assert isinstance(msg, HTTPMessage)
+        assert msg.status == "success"
 
     @staticmethod
     def test_206_valid():
-        assert isinstance(HTTP_MSG_MAPPING[206], HTTPMessage)
+        msg = HTTP_MSG_MAPPING[206]
+        assert isinstance(msg, HTTPMessage)
+        assert msg.status == "success"
 
     @staticmethod
     def test_207_valid():
-        assert isinstance(HTTP_MSG_MAPPING[207], HTTPMessage)
+        msg = HTTP_MSG_MAPPING[207]
+        assert isinstance(msg, HTTPMessage)
+        assert msg.status == "success"
 
     @staticmethod
     def test_208_valid():
-        assert isinstance(HTTP_MSG_MAPPING[208], HTTPMessage)
+        msg = HTTP_MSG_MAPPING[208]
+        assert isinstance(msg, HTTPMessage)
+        assert msg.status == "success"
 
     @staticmethod
     def test_226_valid():
-        assert isinstance(HTTP_MSG_MAPPING[226], HTTPMessage)
+        msg = HTTP_MSG_MAPPING[226]
+        assert isinstance(msg, HTTPMessage)
+        assert msg.status == "success"
 
     @staticmethod
     def test_300_valid():
-        assert isinstance(HTTP_MSG_MAPPING[300], HTTPMessage)
+        msg = HTTP_MSG_MAPPING[300]
+        assert isinstance(msg, HTTPMessage)
+        assert msg.status == "redirect"
 
     @staticmethod
     def test_301_valid():
-        assert isinstance(HTTP_MSG_MAPPING[301], HTTPMessage)
+        msg = HTTP_MSG_MAPPING[301]
+        assert isinstance(msg, HTTPMessage)
+        assert msg.status == "redirect"
 
     @staticmethod
     def test_302_valid():
-        assert isinstance(HTTP_MSG_MAPPING[302], HTTPMessage)
+        msg = HTTP_MSG_MAPPING[302]
+        assert isinstance(msg, HTTPMessage)
+        assert msg.status == "redirect"
 
     @staticmethod
     def test_303_valid():
-        assert isinstance(HTTP_MSG_MAPPING[303], HTTPMessage)
+        msg = HTTP_MSG_MAPPING[303]
+        assert isinstance(msg, HTTPMessage)
+        assert msg.status == "redirect"
 
     @staticmethod
     def test_304_valid():
-        assert isinstance(HTTP_MSG_MAPPING[304], HTTPMessage)
+        msg = HTTP_MSG_MAPPING[304]
+        assert isinstance(msg, HTTPMessage)
+        assert msg.status == "redirect"
 
     @staticmethod
     def test_305_valid():
-        assert isinstance(HTTP_MSG_MAPPING[305], HTTPMessage)
+        msg = HTTP_MSG_MAPPING[305]
+        assert isinstance(msg, HTTPMessage)
+        assert msg.status == "redirect"
 
     @staticmethod
     def test_307_valid():
-        assert isinstance(HTTP_MSG_MAPPING[307], HTTPMessage)
+        msg = HTTP_MSG_MAPPING[307]
+        assert isinstance(msg, HTTPMessage)
+        assert msg.status == "redirect"
 
     @staticmethod
     def test_308_valid():
-        assert isinstance(HTTP_MSG_MAPPING[308], HTTPMessage)
+        msg = HTTP_MSG_MAPPING[308]
+        assert isinstance(msg, HTTPMessage)
+        assert msg.status == "redirect"
 
     @staticmethod
     def test_400_valid():
-        assert isinstance(HTTP_MSG_MAPPING[400], HTTPMessage)
+        msg = HTTP_MSG_MAPPING[400]
+        assert isinstance(msg, HTTPMessage)
+        assert msg.status == "error"
 
     @staticmethod
     def test_401_valid():
-        assert isinstance(HTTP_MSG_MAPPING[401], HTTPMessage)
+        msg = HTTP_MSG_MAPPING[401]
+        assert isinstance(msg, HTTPMessage)
+        assert msg.status == "error"
 
     @staticmethod
     def test_402_valid():
-        assert isinstance(HTTP_MSG_MAPPING[402], HTTPMessage)
+        msg = HTTP_MSG_MAPPING[402]
+        assert isinstance(msg, HTTPMessage)
+        assert msg.status == "error"
 
     @staticmethod
     def test_403_valid():
-        assert isinstance(HTTP_MSG_MAPPING[403], HTTPMessage)
+        msg = HTTP_MSG_MAPPING[403]
+        assert isinstance(msg, HTTPMessage)
+        assert msg.status == "error"
 
     @staticmethod
     def test_404_valid():
-        assert isinstance(HTTP_MSG_MAPPING[404], HTTPMessage)
+        msg = HTTP_MSG_MAPPING[404]
+        assert isinstance(msg, HTTPMessage)
+        assert msg.status == "error"
 
     @staticmethod
     def test_405_valid():
-        assert isinstance(HTTP_MSG_MAPPING[405], HTTPMessage)
+        msg = HTTP_MSG_MAPPING[405]
+        assert isinstance(msg, HTTPMessage)
+        assert msg.status == "error"
 
     @staticmethod
     def test_406_valid():
-        assert isinstance(HTTP_MSG_MAPPING[406], HTTPMessage)
+        msg = HTTP_MSG_MAPPING[406]
+        assert isinstance(msg, HTTPMessage)
+        assert msg.status == "error"
 
     @staticmethod
     def test_407_valid():
-        assert isinstance(HTTP_MSG_MAPPING[407], HTTPMessage)
+        msg = HTTP_MSG_MAPPING[407]
+        assert isinstance(msg, HTTPMessage)
+        assert msg.status == "error"
 
     @staticmethod
     def test_408_valid():
-        assert isinstance(HTTP_MSG_MAPPING[408], HTTPMessage)
+        msg = HTTP_MSG_MAPPING[408]
+        assert isinstance(msg, HTTPMessage)
+        assert msg.status == "error"
 
     @staticmethod
     def test_409_valid():
-        assert isinstance(HTTP_MSG_MAPPING[409], HTTPMessage)
+        msg = HTTP_MSG_MAPPING[409]
+        assert isinstance(msg, HTTPMessage)
+        assert msg.status == "error"
 
     @staticmethod
     def test_410_valid():
-        assert isinstance(HTTP_MSG_MAPPING[410], HTTPMessage)
+        msg = HTTP_MSG_MAPPING[410]
+        assert isinstance(msg, HTTPMessage)
+        assert msg.status == "error"
 
     @staticmethod
     def test_411_valid():
-        assert isinstance(HTTP_MSG_MAPPING[411], HTTPMessage)
+        msg = HTTP_MSG_MAPPING[411]
+        assert isinstance(msg, HTTPMessage)
+        assert msg.status == "error"
 
     @staticmethod
     def test_412_valid():
-        assert isinstance(HTTP_MSG_MAPPING[412], HTTPMessage)
+        msg = HTTP_MSG_MAPPING[412]
+        assert isinstance(msg, HTTPMessage)
+        assert msg.status == "error"
 
     @staticmethod
     def test_413_valid():
-        assert isinstance(HTTP_MSG_MAPPING[413], HTTPMessage)
+        msg = HTTP_MSG_MAPPING[413]
+        assert isinstance(msg, HTTPMessage)
+        assert msg.status == "error"
 
     @staticmethod
     def test_414_valid():
-        assert isinstance(HTTP_MSG_MAPPING[414], HTTPMessage)
+        msg = HTTP_MSG_MAPPING[414]
+        assert isinstance(msg, HTTPMessage)
+        assert msg.status == "error"
 
     @staticmethod
     def test_415_valid():
-        assert isinstance(HTTP_MSG_MAPPING[415], HTTPMessage)
+        msg = HTTP_MSG_MAPPING[415]
+        assert isinstance(msg, HTTPMessage)
+        assert msg.status == "error"
 
     @staticmethod
     def test_416_valid():
-        assert isinstance(HTTP_MSG_MAPPING[416], HTTPMessage)
+        msg = HTTP_MSG_MAPPING[416]
+        assert isinstance(msg, HTTPMessage)
+        assert msg.status == "error"
 
     @staticmethod
     def test_417_valid():
-        assert isinstance(HTTP_MSG_MAPPING[417], HTTPMessage)
+        msg = HTTP_MSG_MAPPING[417]
+        assert isinstance(msg, HTTPMessage)
+        assert msg.status == "error"
 
     @staticmethod
     def test_418_valid():
-        assert isinstance(HTTP_MSG_MAPPING[418], HTTPMessage)
+        msg = HTTP_MSG_MAPPING[418]
+        assert isinstance(msg, HTTPMessage)
+        assert msg.status == "error"
 
     @staticmethod
     def test_421_valid():
-        assert isinstance(HTTP_MSG_MAPPING[421], HTTPMessage)
+        msg = HTTP_MSG_MAPPING[421]
+        assert isinstance(msg, HTTPMessage)
+        assert msg.status == "error"
 
     @staticmethod
     def test_422_valid():
-        assert isinstance(HTTP_MSG_MAPPING[422], HTTPMessage)
+        msg = HTTP_MSG_MAPPING[422]
+        assert isinstance(msg, HTTPMessage)
+        assert msg.status == "error"
 
     @staticmethod
     def test_423_valid():
-        assert isinstance(HTTP_MSG_MAPPING[423], HTTPMessage)
+        msg = HTTP_MSG_MAPPING[423]
+        assert isinstance(msg, HTTPMessage)
+        assert msg.status == "error"
 
     @staticmethod
     def test_424_valid():
-        assert isinstance(HTTP_MSG_MAPPING[424], HTTPMessage)
+        msg = HTTP_MSG_MAPPING[424]
+        assert isinstance(msg, HTTPMessage)
+        assert msg.status == "error"
 
     @staticmethod
     def test_425_valid():
-        assert isinstance(HTTP_MSG_MAPPING[425], HTTPMessage)
+        msg = HTTP_MSG_MAPPING[425]
+        assert isinstance(msg, HTTPMessage)
+        assert msg.status == "error"
 
     @staticmethod
     def test_426_valid():
-        assert isinstance(HTTP_MSG_MAPPING[426], HTTPMessage)
+        msg = HTTP_MSG_MAPPING[426]
+        assert isinstance(msg, HTTPMessage)
+        assert msg.status == "error"
 
     @staticmethod
     def test_428_valid():
-        assert isinstance(HTTP_MSG_MAPPING[428], HTTPMessage)
+        msg = HTTP_MSG_MAPPING[428]
+        assert isinstance(msg, HTTPMessage)
+        assert msg.status == "error"
 
     @staticmethod
     def test_429_valid():
-        assert isinstance(HTTP_MSG_MAPPING[429], HTTPMessage)
+        msg = HTTP_MSG_MAPPING[429]
+        assert isinstance(msg, HTTPMessage)
+        assert msg.status == "error"
 
     @staticmethod
     def test_431_valid():
-        assert isinstance(HTTP_MSG_MAPPING[431], HTTPMessage)
+        msg = HTTP_MSG_MAPPING[431]
+        assert isinstance(msg, HTTPMessage)
+        assert msg.status == "error"
 
     @staticmethod
     def test_451_valid():
-        assert isinstance(HTTP_MSG_MAPPING[451], HTTPMessage)
+        msg = HTTP_MSG_MAPPING[451]
+        assert isinstance(msg, HTTPMessage)
+        assert msg.status == "error"
 
     @staticmethod
     def test_500_valid():
-        assert isinstance(HTTP_MSG_MAPPING[500], HTTPMessage)
+        msg = HTTP_MSG_MAPPING[500]
+        assert isinstance(msg, HTTPMessage)
+        assert msg.status == "error"
 
     @staticmethod
     def test_501_valid():
-        assert isinstance(HTTP_MSG_MAPPING[501], HTTPMessage)
+        msg = HTTP_MSG_MAPPING[501]
+        assert isinstance(msg, HTTPMessage)
+        assert msg.status == "error"
 
     @staticmethod
     def test_502_valid():
-        assert isinstance(HTTP_MSG_MAPPING[502], HTTPMessage)
+        msg = HTTP_MSG_MAPPING[502]
+        assert isinstance(msg, HTTPMessage)
+        assert msg.status == "error"
 
     @staticmethod
     def test_503_valid():
-        assert isinstance(HTTP_MSG_MAPPING[503], HTTPMessage)
+        msg = HTTP_MSG_MAPPING[503]
+        assert isinstance(msg, HTTPMessage)
+        assert msg.status == "error"
 
     @staticmethod
     def test_504_valid():
-        assert isinstance(HTTP_MSG_MAPPING[504], HTTPMessage)
+        msg = HTTP_MSG_MAPPING[504]
+        assert isinstance(msg, HTTPMessage)
+        assert msg.status == "error"
 
     @staticmethod
     def test_505_valid():
-        assert isinstance(HTTP_MSG_MAPPING[505], HTTPMessage)
+        msg = HTTP_MSG_MAPPING[505]
+        assert isinstance(msg, HTTPMessage)
+        assert msg.status == "error"
 
     @staticmethod
     def test_506_valid():
-        assert isinstance(HTTP_MSG_MAPPING[506], HTTPMessage)
+        msg = HTTP_MSG_MAPPING[506]
+        assert isinstance(msg, HTTPMessage)
+        assert msg.status == "error"
 
     @staticmethod
     def test_507_valid():
-        assert isinstance(HTTP_MSG_MAPPING[507], HTTPMessage)
+        msg = HTTP_MSG_MAPPING[507]
+        assert isinstance(msg, HTTPMessage)
+        assert msg.status == "error"
 
     @staticmethod
     def test_508_valid():
-        assert isinstance(HTTP_MSG_MAPPING[508], HTTPMessage)
+        msg = HTTP_MSG_MAPPING[508]
+        assert isinstance(msg, HTTPMessage)
+        assert msg.status == "error"
 
     @staticmethod
     def test_510_valid():
-        assert isinstance(HTTP_MSG_MAPPING[510], HTTPMessage)
+        msg = HTTP_MSG_MAPPING[510]
+        assert isinstance(msg, HTTPMessage)
+        assert msg.status == "error"
 
     @staticmethod
     def test_511_valid():
-        assert isinstance(HTTP_MSG_MAPPING[511], HTTPMessage)
+        msg = HTTP_MSG_MAPPING[511]
+        assert isinstance(msg, HTTPMessage)
+        assert msg.status == "error"
